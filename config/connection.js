0ffe923d2157e88328073d35779111aa5f2791a0
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-06-11 20:08:32 -0500

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //set up mysql connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "TimpfSQL81013##Torchwood",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    })
+};
+
+
 
 //make the connection
 connection.connect(function(err) {
